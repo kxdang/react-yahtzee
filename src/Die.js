c48c4a1f304724efb8ea207d210f1ef81d03b083
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-17 14:50:08 -0400

    fixing rolling animation

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numberWord: ["one", "two", "three", "four", "five", "six"],
+    val: 1
+  };
   constructor(props) {
     super(props);
     this.handleDieClick = this.handleDieClick.bind(this);
@@ -11,14 +15,14 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
+    const { numberWord, locked, val, disabled, isRolling } = this.props;
+    let dice = `Die fas fa-dice-${numberWord[val - 1]} fa-5x `;
+
+    if (locked) dice += "Die-locked";
+    if (isRolling) dice += "Die-rolling";
+
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.handleDieClick}
-      >
-        {this.props.val}
-      </button>
+      <i className={dice} onClick={this.handleDieClick} disabled={disabled} />
     );
   }
 }
